1824e75a3e209ff348248f2a438b652aec659ccb
Rubén Bernárdez <rubenbp@gmail.com>
2011-07-09 17:30:18 +0200

    Verify status in moves

--- unit.py
+++ unit.py
@@ -8,8 +8,7 @@ __author__ = 'rubenbp'
 class RobotTest(unittest.TestCase):
     def setUp(self):
         self.server_proxy = spy(ServerProxy())
-        when(self.server_proxy.move).then_return(10)
-
+        when(self.server_proxy.move).then_return({"OK", 10})
         self.robot = Robot("robocop",self.server_proxy)
 
     def test_initial_score_is_zero(self):
@@ -32,12 +31,39 @@ class RobotTest(unittest.TestCase):
         server_proxy_mock = mock(ServerProxy())
         expect_call(server_proxy_mock.init)
         expect_call(server_proxy_mock.move
-            ).returning(10).times(2)
+            ).returning({"OK",10}).times(2)
 
         self.robot = Robot("robocop", server_proxy_mock)
         self.robot.start(total_moves = 2)
 
         server_proxy_mock.assert_that_is_satisfied()
+        assert_that(self.robot.score, equal_to(20))
+
+    def test_game_over_in_a_move(self):
+        server_proxy_mock = mock(ServerProxy())
+        expect_call(server_proxy_mock.init)
+        expect_call(server_proxy_mock.move
+            ).returning({"GameOver",0})
+
+        self.robot = Robot("robocop", server_proxy_mock)
+        self.robot.start(total_moves = 2)
+
+        server_proxy_mock.assert_that_is_satisfied()
+        assert_that(self.robot.status, equal_to("GameOver"))
+        assert_that(self.robot.score, equal_to(0))
+
+    def test_win_in_a_move(self):
+        server_proxy_mock = mock(ServerProxy())
+        expect_call(server_proxy_mock.init)
+        expect_call(server_proxy_mock.move
+            ).returning({"YouWin",10})
+
+        self.robot = Robot("robocop", server_proxy_mock)
+        self.robot.start(total_moves = 2)
+
+        server_proxy_mock.assert_that_is_satisfied()
+        assert_that(self.robot.status, equal_to("YouWin"))
+        assert_that(self.robot.score, equal_to(10))
 
 class ServerProxy():
     def init(self, robot_name):
@@ -51,9 +77,19 @@ class Robot():
         self.name = name
         self.server_proxy = server_proxy
         self.score = 0
+        self.status = ""
         
     def start(self, total_moves):
         self.server_proxy.init(self.name)
         for x in range(total_moves):
-            self.score += self.server_proxy.move(self.name, 20)
+            move_score, status = self.server_proxy.move(self.name, 20)
+            self.status = status
 
+            if status == "GameOver":
+                break
+
+            if status == "YouWin":
+                self.score += move_score
+                break
+
+            self.score += move_score
