28970868d34ac3e6396d38ec6818b7856551237c
Rubén Bernárdez <rubenbp@gmail.com>
2011-07-10 16:42:02 +0200

    Replacing code to support new version of pyDoubles

--- unit.py
+++ unit.py
@@ -2,6 +2,7 @@ import unittest
 from hamcrest.core.assert_that import assert_that
 from hamcrest.core.core.allof import all_of
 from hamcrest.core.core.isequal import equal_to
+from hamcrest.core.core.isnot import is_not
 from hamcrest.library.number.ordering_comparison import greater_than_or_equal_to, less_than_or_equal_to
 from pyDoubles.framework import *
 
@@ -10,7 +11,7 @@ __author__ = 'rubenbp'
 class RobotTest(unittest.TestCase):
     def setUp(self):
         self.server_proxy = spy(ServerProxy())
-        when(self.server_proxy.move).then_return({"OK", 10})
+        when(self.server_proxy.move).then_return(("OK", 10))
 
         next_cell_calculator = stub(NextCellCalculator(0, 100))
         when(next_cell_calculator.next).then_return(5)
@@ -36,29 +37,26 @@ class RobotTest(unittest.TestCase):
 
 class RobotMovesTests(unittest.TestCase):
     def setUp(self):
-        self.server_proxy_mock = mock(ServerProxy())
-        expect_call(self.server_proxy_mock.init)
+        self.server_proxy_stub = stub(ServerProxy)
 
         next_cell_calculator = stub(NextCellCalculator(0, 100))
         when(next_cell_calculator.next).then_return(5)
 
         self.robot = Robot(
-            "robocop", self.server_proxy_mock, next_cell_calculator)
-
-    def tearDown(self):
-        self.server_proxy_mock.assert_that_is_satisfied()
+            "robocop", self.server_proxy_stub, next_cell_calculator)
 
     def test_move_to_cell_severals_times(self):
-        expect_call(self.server_proxy_mock.move
-            ).returning({"OK",10}).times(2)
+        server_proxy_spy = spy(ServerProxy())
+        when(server_proxy_spy.move).then_return(("OK", 10))
+        self.robot.server_proxy = server_proxy_spy
 
         self.robot.start(total_moves = 2)
 
+        assert_that_method(server_proxy_spy.move).was_called().times(2)
         assert_that(self.robot.score, equal_to(20))
 
     def test_game_over_in_a_move(self):
-        expect_call(self.server_proxy_mock.move
-            ).returning({"GameOver",0})
+        when(self.server_proxy_stub.move).then_return(("GameOver", 0))
 
         self.robot.start(total_moves = 2)
 
@@ -66,8 +64,7 @@ class RobotMovesTests(unittest.TestCase):
         assert_that(self.robot.score, equal_to(0))
 
     def test_win_in_a_move(self):
-        expect_call(self.server_proxy_mock.move
-            ).returning({"YouWin",10})
+        when(self.server_proxy_stub.move).then_return(("YouWin",10))
 
         self.robot.start(total_moves = 2)
 
@@ -76,11 +73,9 @@ class RobotMovesTests(unittest.TestCase):
 
     def test_robot_use_next_cell_calculator(self):
         next_cell_calculator = mock(NextCellCalculator(0, 100))
-        expect_call(next_cell_calculator.next
-            ).returning(10)
+        expect_call(next_cell_calculator.next).returning(10)
         self.robot.next_cell_calculator = next_cell_calculator
-        expect_call(self.server_proxy_mock.move
-            ).returning({"OK",10})
+        when(self.server_proxy_stub.move).then_return(("OK",10))
 
         self.robot.start(total_moves = 1)
 
@@ -90,18 +85,31 @@ class NextCellCalculatorTests(unittest.TestCase):
 
     def test_return_next_cell_in_range(self):
         next_cell_calculator = NextCellCalculator(0, 100)
-        assert_that(
-            next_cell_calculator.next(),
-            all_of(greater_than_or_equal_to(0),
-                    less_than_or_equal_to(100)))
+        for x in range(999):
+            assert_that(
+                next_cell_calculator.next(),
+                all_of(greater_than_or_equal_to(0),
+                        less_than_or_equal_to(100)))
+
+    def test_dont_return_twice_the_same_number(self):
+        next_cell_calculator = NextCellCalculator(0, 100)
+
+        for x in range(100):
+            assert_that(
+                next_cell_calculator.next(),
+                is_not(equal_to(next_cell_calculator.next())))
 
 class NextCellCalculator():
     def __init__(self, min_cell, max_cell):
         self.min_cell = min_cell
         self.max_cell = max_cell
-        
+        self.last_cell = self.min_cell
+
     def next(self):
-        return 0
+        self.last_cell += 1
+        if self.last_cell > self.max_cell:
+            self.last_cell = self.min_cell
+        return self.last_cell
 
 class ServerProxy():
     def init(self, robot_name):
@@ -122,7 +130,7 @@ class Robot():
         self.server_proxy.init(self.name)
         for x in range(total_moves):
             next_cell = self.next_cell_calculator.next()
-            move_score, status = self.server_proxy.move(
+            status, move_score = self.server_proxy.move(
                                     self.name, next_cell)
             self.status = status
 
